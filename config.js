477a48c73f187c6efa72fc9bf2482dbdd8839b68
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-17 03:55:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-var api = 'http://45.32.232.11:32348';
-var blockTargetInterval = 240;
+var api = 'http://127.0.0.1:24091';
+var blockTargetInterval = 60;
 var coinUnits = 1000000000000;
-var symbol = 'KRB';
+var symbol = 'XSM';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
